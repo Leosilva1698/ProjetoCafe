d5189047322fa3bfd2ff7ba69984205362852633
Leosilva1698 <133409205+Leosilva1698@users.noreply.github.com>
2024-02-06 18:25:22 -0300

    Tratativa de excecao funcionario

--- Controllers/FuncionarioController.cs
+++ Controllers/FuncionarioController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using ProjetoCafe.Data;
 using ProjetoCafe.DTOS;
 using ProjetoCafe.Models;
+using ProjetoCafe.Exceptions;
 
 namespace ProjetoCafe.Controllers
 {
@@ -12,108 +13,201 @@ namespace ProjetoCafe.Controllers
         private readonly CafeDBContext _context;
 
         public FuncionarioController()
-        {   
+        {
             _context = new CafeDBContext();
         }
 
         [HttpGet]
         public IActionResult Get()
         {
-            var funcionarios = _context.Funcionario.ToList();
-            List<FuncionarioDTO> listFucionarios = new List<FuncionarioDTO>();
-
-            foreach (var f in funcionarios)
+            try
             {
-                listFucionarios.Add(new FuncionarioDTO(f));
+                var funcionarios = _context.Funcionario.ToList();
+                List<FuncionarioDTO> listFucionarios = new List<FuncionarioDTO>();
+
+                foreach (var f in funcionarios)
+                {
+                    listFucionarios.Add(new FuncionarioDTO(f));
+                }
+
+                return Ok(listFucionarios);
+            }
+            catch (InvalidOperationException e)
+            {
+                _ = new CafeException(e.Message);
+                return BadRequest("Erro ao se comunicar com o Banco de Dados");
+            }
+            catch (Exception e)
+            {
+                _ = new CafeException(e.Message);
+                return BadRequest("Sistema fora do Ar!");
             }
 
-            return Ok(listFucionarios);
         }
 
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            var funcionario = _context.Funcionario
+            try
+            {
+                var funcionario = _context.Funcionario
                 .FirstOrDefault(f => f.FuncionarioID == id);
 
-            if (funcionario == null)
-                return NotFound();
-        
-            return Ok(new FuncionarioDTO(funcionario));
+                if (funcionario == null)
+                    return NotFound("Funcionario não encontrado.");
+
+                return Ok(new FuncionarioDTO(funcionario));
+            }
+            catch (InvalidOperationException e)
+            {
+                _ = new CafeException(e.Message);
+                return BadRequest("Erro ao se comunicar com o Banco de Dados");
+            }
+            catch (Exception e)
+            {
+                _ = new CafeException(e.Message);
+                return BadRequest("Sistema fora do Ar!");
+            }
+
+
+
         }
 
         [HttpGet("Gorjetas/{notafiscalId}")]
         public IActionResult funcionariosGorjeta(int notafiscalId)
         {
-            var notafiscal = _context.NotaFiscal.FirstOrDefault(nf => nf.NotaFiscalID == notafiscalId);
-
-            if (notafiscal == null)
-                return NotFound();
-
-            var funcionarios = _context.NotaFiscal
-                .Where(nf => nf.NotaFiscalID == notafiscalId)
-                .Join(
-                    _context.Comanda,
-                    nota => nota.ComandaID,
-                    comanda => comanda.ComandaID,
-                    (nota, comanda) => new {comanda}
-                )
-                .Join(
-                    _context.Pedido,
-                    comanda => comanda.comanda.ComandaID,
-                    pedido => pedido.ComandaID,
-                    (comanda, pedido) => new {pedido}
-                )
-                .Join(
-                    _context.Funcionario,
-                    pedido => pedido.pedido.FuncionarioID,
-                    funcionario => funcionario.FuncionarioID,
-                    (pedido, funcionario) => new {funcionario.Nome}
-                )
-                .Select(resultado => resultado.Nome)
-                .ToList();
-
-            int quantFuncionarios = funcionarios.Count;
-
-            string display = "";
-            if (notafiscal.TaxaServico)
+            try
             {
-                double gorjeta = notafiscal.ValorTotal * 0.1 / quantFuncionarios;
+                var notafiscal = _context.NotaFiscal.FirstOrDefault(nf => nf.NotaFiscalID == notafiscalId);
 
-                foreach (var nome in funcionarios)
+                if (notafiscal == null)
+                    return NotFound("Nota Fiscal não encontrada");
+
+
+
+                var funcionarios = _context.NotaFiscal
+                    .Where(nf => nf.NotaFiscalID == notafiscalId)
+                    .Join(
+                        _context.Comanda,
+                        nota => nota.ComandaID,
+                        comanda => comanda.ComandaID,
+                        (nota, comanda) => new { comanda }
+                    )
+                    .Join(
+                        _context.Pedido,
+                        comanda => comanda.comanda.ComandaID,
+                        pedido => pedido.ComandaID,
+                        (comanda, pedido) => new { pedido }
+                    )
+                    .Join(
+                        _context.Funcionario,
+                        pedido => pedido.pedido.FuncionarioID,
+                        funcionario => funcionario.FuncionarioID,
+                        (pedido, funcionario) => new { funcionario.Nome }
+                    )
+                    .Select(resultado => resultado.Nome)
+                    .ToList();
+
+                int quantFuncionarios = funcionarios.Count;
+
+                string display = "";
+                if (notafiscal.TaxaServico)
                 {
-                    display += $"{nome} recebeu {gorjeta} R$ de gorjeta\n";
-                }
-            }
+                    double gorjeta = notafiscal.ValorTotal * 0.1 / quantFuncionarios;
 
-            return Ok(display);
+                    foreach (var nome in funcionarios)
+                    {
+                        display += $"{nome} recebeu {gorjeta} R$ de gorjeta\n";
+                    }
+                }
+                else
+                {
+                    display += "Essa nota não gerou gorgeta";
+                }
+                return Ok(display);
+            }
+            catch (InvalidOperationException e)
+            {
+                _ = new CafeException(e.Message);
+                return BadRequest("Erro ao se comunicar com o Banco de Dados");
+            }
+            catch (Exception e)
+            {
+                _ = new CafeException(e.Message);
+                return BadRequest("Sistema fora do Ar!");
+            }
         }
 
         [HttpPost]
         public IActionResult Post([FromBody] FuncionarioDTO novoFuncionario)
         {
-            FuncionarioModel funcionario = new FuncionarioModel(novoFuncionario);
-            _context.Funcionario.Add(funcionario);
-            _context.SaveChanges();
-            
-            novoFuncionario.setFuncionarioId(funcionario.FuncionarioID);
-            return Ok(novoFuncionario);
+            try
+            {
+                FuncionarioModel funcionario = new FuncionarioModel(novoFuncionario);
+                _context.Funcionario.Add(funcionario);
+                _context.SaveChanges();
+
+                novoFuncionario.setFuncionarioId(funcionario.FuncionarioID);
+                return Ok(novoFuncionario);
+            }
+            catch (InvalidOperationException e)
+            {
+                _ = new CafeException(e.Message);
+                return BadRequest("Erro ao se comunicar com o Banco de Dados");
+            }
+            catch (FormatException e)
+            {
+                _ = new CafeException(e.Message);
+                return BadRequest("Formato inválido");
+            }
+            catch (CafeException e)
+            {
+                return BadRequest(e.avisoErro);
+            }
+            catch (Exception e)
+            {
+                _ = new CafeException(e.Message);
+                return BadRequest("Sistema fora do Ar!");
+            }
         }
 
         [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody] FuncionarioDTO edicoes)
         {
-            var funcionario = _context.Funcionario
-                .FirstOrDefault(f => f.FuncionarioID == id);
+            try
+            {
+                var funcionario = _context.Funcionario
+                    .FirstOrDefault(f => f.FuncionarioID == id);
 
-            if (funcionario == null)
-                return NotFound();
-            
-            funcionario = edicoes.editFuncionario(funcionario);
+                if (funcionario == null)
+                    return NotFound("Funcionario não encontrado");
 
-            _context.Entry(funcionario).CurrentValues.SetValues(funcionario);
-            _context.SaveChanges();
-            return Ok(edicoes);
+                funcionario = edicoes.editFuncionario(funcionario);
+
+                _context.Entry(funcionario).CurrentValues.SetValues(funcionario);
+                _context.SaveChanges();
+                return Ok(edicoes);
+            }
+            catch (InvalidOperationException e)
+            {
+                _ = new CafeException(e.Message);
+                return BadRequest("Erro ao se comunicar com o Banco de Dados");
+            }
+            catch (FormatException e)
+            {
+                _ = new CafeException(e.Message);
+                return BadRequest("Formato inválido");
+            }
+            catch (CafeException e)
+            {
+                return BadRequest(e.avisoErro);
+            }
+            catch (Exception e)
+            {
+                _ = new CafeException(e.Message);
+                return BadRequest("Sistema fora do Ar!");
+            }
+
         }
 
         [HttpDelete("{id}")]
@@ -123,19 +217,25 @@ namespace ProjetoCafe.Controllers
             {
                 var funcionario = _context.Funcionario
                     .FirstOrDefault(f => f.FuncionarioID == id);
-                
+
                 if (funcionario == null)
                 {
-                    return NotFound();
+                    return NotFound("Funcionario não encontrado");
                 }
 
                 _context.Funcionario.Remove(funcionario);
                 _context.SaveChanges();
-                return Ok();
+                return Ok($"Funcionario {funcionario.Nome} excluido!");
+            }
+            catch (InvalidOperationException e)
+            {
+                _ = new CafeException(e.Message);
+                return BadRequest("Erro ao se comunicar com o Banco de Dados");
             }
             catch (Exception e)
             {
-                return BadRequest(e.Message);
+                _ = new CafeException(e.Message);
+                return BadRequest("Sistema fora do Ar!");
             }
         }
     }
--- Models/FuncionarioModel.cs
+++ Models/FuncionarioModel.cs
@@ -1,14 +1,34 @@
 using ProjetoCafe.DTOS;
+using ProjetoCafe.Exceptions;
 
 namespace ProjetoCafe.Models
 
 {
     public class FuncionarioModel
     {
+        private string? _Nome;
         public int FuncionarioID { get; set; }
-        public string? Nome { get; set; }
+        public string? Nome
+        {
+            get
+            {
+                return _Nome;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || value.Trim() == "")
+                {
+                    throw new CafeException("Nome do Funcionario é obrigatorio");
+                }
+                else if (value.Length > 60)
+                {
+                    throw new CafeException("Nome do Funcionario excedeu o limete de 60 caracteres");
+                }
+                _Nome = value;
+            }
+        }
 
-        public FuncionarioModel(){ }
+        public FuncionarioModel() { }
 
         public FuncionarioModel(FuncionarioDTO funcionario)
         {
